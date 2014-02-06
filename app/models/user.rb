aed9c4246f5c35350d90a78e00a075ae423feb26
Tara Roys <tlroys@gmail.com>
2014-02-06 10:27:17 -0600

    Bugfix: deleted extra comma

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
   attr_reader :entered_password
 
-  has_many :proficiencies,
+  has_many :proficiencies
   has_many :skills, :through => :proficiencies
 
   validates :name, :length => { :minimum => 3, :message => "must be at least 3 characters, fool!" }
