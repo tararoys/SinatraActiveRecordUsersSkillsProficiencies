5f1f1b5d276ad0aead0db789613f377fea3b6f41
Tara Roys <tlroys@gmail.com>
2014-02-06 10:55:48 -0600

    Didn't get far. Moving on

--- app/controllers/users.rb
+++ app/controllers/users.rb
@@ -3,6 +3,12 @@ get '/users/:id' do
 end
 
 get '/users/:id/proficiencies/new' do
-  "user goes here to add proficiencies"
+  erb :users
 end
+
+post '/users/:id/proficiencies/new' do
+  Proficiency.new(user_id: User.all[3].id, skill_id: Skill.all[0].id, years_experience: 4, formal_education: true);
+  redirect '/'
+end
+
 
